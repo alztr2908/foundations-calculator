a85786ca119ea97eb3e21b150ce5431f7a000b27
alztr2908@gmail.com <alztr2908@gmail.com>
2024-08-31 20:25:50 +0800

    creating initial logic for buttons

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+const buttonGroup = document.querySelectorAll("button");
+
+buttonGroup.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.currentTarget.classList.value) {
+      case "number-button":
+        console.log("number");
+        break;
+      case "operator-button":
+        console.log("operator");
+        break;
+      case "utils-button":
+        console.log("utils");
+        break;
+    }
+  });
+});
